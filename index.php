b56dc7fc20ec2c1fd4501fb500f9f69f7baf155b
Max <maximusdyer97@gmail.com>
2021-09-28 23:50:11 +0100

    index.php

--- index.php
+++ index.php
@@ -8,27 +8,44 @@ require_once 'includes/functions.php';
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/normalize.css">
-<!--    <link rel="stylesheet" href="css/stylesheet.css">-->
-<head>
-    <title> Wonders of the World Collection </title>
-</head>
-<body>
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@200&display=swap" rel="stylesheet">
 
+<body class="container">
+     <h1> Wonders of the World Collection </h1>
+    <div class="container1">
+        <nav class="navbar">
+            <div class="hamburger-menu">
+                <div class="line line-1"></div>
+                <div class="line line-2"></div>
+                <div class="line line-3"></div>
+            </div>
+
+            <ul class="nav-list">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Wonders of World</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Facts</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">pls work</a>
+                </li>
+            </ul>
+
+
+        </nav>
+    </div>
 
     <section>
-        <h1> Wonders of the World</h1>
         <?php
         $wonders = getWonders($db);
         echo formatWonders($wonders);
-
-        $allWonders .= '<img src="';
-        $path = getImagePath($wonders['image']);
-        $allWonders .= ($path . '" alt="Picture of ' . $wonders['name'] . '">');
         ?>
-
     </section>
-
-
 
 
 </body>
